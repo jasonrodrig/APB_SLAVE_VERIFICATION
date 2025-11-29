fc35bddabf6a042250984c8ede895aa1e5aa15f4
Jason Linus Rodrigues <jasonlinus@feserver.mirafra.com>
2025-11-29 17:44:19 +0530

    modified driver and passive monitor code

--- src/apb_master_passive_monitor.sv
+++ src/apb_master_passive_monitor.sv
@@ -32,18 +32,67 @@ class apb_master_passive_monitor extends uvm_monitor;
 			`uvm_fatal("NOVIF",{"virtual interface must be set for:APB_MASTER MONITOR INTERFACE ",get_full_name(),".vif"});
 	endfunction
 
-	//------------------------------------------------------//
-	//    capturing the output signals from the interface   //  
-	//------------------------------------------------------//
+	//--------------------------------------------------------//
+	// capturing the output signals from the interface during //
+	//			the transistion from IDLE-> SETUP -> ACCESS       //  
+	//--------------------------------------------------------//
 
 	task run_phase(uvm_phase phase);
 		repeat(3) @(vif.apb_master_monitor_cb);
 		forever begin
-			repeat(1) @(vif.apb_master_monitor_cb);
-			seq.PREADY  = vif.apb_master_monitor_cb.PREADY;
+		//	repeat(1) @(vif.apb_master_monitor_cb);
+		 if( !vif.apb_master_monitor_cb.PSELX || !vif.apb_master_monitor_cb.PRESETN )
+				idle_state();
+		 else if( vif.apb_master_monitor_cb.PSELX && !vif.apb_master_monitor_cb.PENABLE && vif.apb_master_monitor_cb.PRESETN ) 
+			begin
+				setup_state();
+				access_state();
+			end 
+		end
+	endtask
+
+  //-------------------------------------------------------------------//
+	//   capturing the output signals from the interface in idle state   //  
+	//-------------------------------------------------------------------//
+
+	task idle_state();
+		repeat(1)@(vif.apb_master_monitor_cb);
+    	seq.PREADY  = vif.apb_master_monitor_cb.PREADY;
 	  	seq.PRDATA  = vif.apb_master_monitor_cb.PRDATA;
       seq.PSLVERR = vif.apb_master_monitor_cb.PSLVERR;
       passive_mon_port.write(seq); 
-		end
+	endtask		
+
+  //-------------------------------------------------------------------//
+	//   capturing the output signals from the interface in setup state  //  
+	//-------------------------------------------------------------------//
+
+	task setup_state();
+		repeat(1)@(vif.apb_master_monitor_cb);
 	endtask
+  
+  //-------------------------------------------------------------------//
+	//   capturing the output signals from the interface in access state //  
+	//-------------------------------------------------------------------//
+
+	task access_state();
+   // not sure where to place the output either before or after 
+   // once the design ccode is shared , need to do trail and error
+
+    seq.PREADY  = vif.apb_master_monitor_cb.PREADY;
+	  seq.PRDATA  = vif.apb_master_monitor_cb.PRDATA;
+    seq.PSLVERR = vif.apb_master_monitor_cb.PSLVERR; 
+  
+		while(!vif.apb_master_monitor_cb.PREADY)
+		begin
+			repeat(1)@(vif.apb_master_monitor_cb);
+		end
+   
+		// not sure when to send the signals to the scoreboard
+		// yet to confirm once design is sent
+		repeat(1)@(vif.apb_master_monitor_cb);
+	  passive_mon_port.write(seq); 
+		//repeat(1)@(vif.apb_master_monitor_cb);
+	endtask
+
 endclass
